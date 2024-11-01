4b4a3af7d30e0f08552e7135d31630a22ddf1baf
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-11-01 01:20:28 -0500

    Finished all the base functionality

---
+++ popup.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const generateResponseBtn = document.getElementById('generate-response-btn');
+    const provideContextBtn = document.getElementById('provice-context-btn');
+    const contextContainer = document.getElementById('context-container');
+    const submitContextBtn = document.getElementById('submit-context-btn');
+
+    provideContextBtn?.addEventListener('click', () => {
+        contextContainer?.style.display === 'none' ? 'block' : 'none';
+    });
+
+    generateResponseBtn?.addEventListener('click', () => {
+        chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'generateResponse', context: ''});
+        });
+    });;
+
+    submitContextBtn?.addEventListener('click', () => {
+        const context = document.getElementById('context-input');
+        chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'generateResponse', context });
+        });
+    });
+})
